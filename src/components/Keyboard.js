9637848984e8ddf037f8536de1405d5fa7e9185a
Kiet Vuong <kiet.vuong@growwithtreehouse.ca>
2025-01-13 20:45:45 -0500

    updated Keyboard model

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 function Keyboard() {
   return (
     <>
-      <Spline scene="https://prod.spline.design/I7rEeHLUR6LUp1ah/scene.spline" />
+      <Spline scene="https://prod.spline.design/I7rEeHLUR6LUp1ah/scene.splinecode" />
     </>
   );
 }
